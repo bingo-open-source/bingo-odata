a9016b97e2e4ad9b4dceb37a64ab71e39da8b487
Calvin Chen <calvin.chen.310@gmail.com>
2013-08-28 13:57:08 +0800

    add test

---
+++ odata-core/src/test/java/bingo/odata/expression/ExpressionsTest.java
@@ -0,0 +1,19 @@
+package bingo.odata.expression;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExpressionsTest {
+
+	@Test
+	public void testParse() {
+		String exprs = "not (name eq 'chenkai') and sex ne 'girl'";
+		Expression ex = Expressions.parse(exprs);
+		ExpressionVisitor visitor = new FilterExpressionVisitor();
+		ex.visit(visitor);
+		System.out.println(visitor.toString());
+	}
+
+
+}
